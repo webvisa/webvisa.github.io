7132c2f91c28685d1028451f7388c4861de26f91
webvisa <33812593+webvisa@users.noreply.github.com>
2017-11-21 10:59:37 +0200

    Commit

--- MyIndivid/mail.php
+++ MyIndivid/mail.php
@@ -12,14 +12,14 @@ $what=$_POST['what'];
 //$mail->SMTPDebug = 3;                               // Enable verbose debug output
 
 $mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp.i.ua';  																							// Specify main and backup SMTP servers
+$mail->Host = 'smtp.gmail.com';  																							// Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'kravchenkovg@i.ua'; // Ваш логин от почты с которой будут отправляться письма
-$mail->Password = 'vov4ik'; // Ваш пароль от почты с которой будут отправляться письма
+$mail->Username = 'test14810@gmail.com'; // Ваш логин от почты с которой будут отправляться письма
+$mail->Password = '1481028Test'; // Ваш пароль от почты с которой будут отправляться письма
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
 
-$mail->setFrom('kravchenkovg@i.ua'); // от кого будет уходить письмо?
+$mail->setFrom('test14810@gmail.com'); // от кого будет уходить письмо?
 $mail->addAddress('kravchenkovg@gmail.com');     // Кому будет уходить письмо 
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
@@ -30,7 +30,7 @@ $mail->addAddress('kravchenkovg@gmail.com');     // Кому будет уход
 $mail->isHTML(true);                                  // Set email format to HTML
 
 $mail->Subject = 'Заявка с сайта MyIndivid';
-$mail->Body    = '' .$name . ' оставил заявку, его телефон ' .$phone. '<br>Почта этого пользователя: ' .$email;
+$mail->Body    = '' .$name . ' оставил заявку, его телефон ' .$phone. '<br>Почта этого пользователя: ' '<br>Что хочет: '  .$what;
 $mail->AltBody = '';
 
 if(!$mail->send()) {
