2cf81c036c7939189082afcee7e40ada4a730129
webvisa <33812593+webvisa@users.noreply.github.com>
2018-01-25 10:19:10 +0200

    Commit

---
+++ Sliding/js/custom.js
@@ -0,0 +1,32 @@
+$(function() {
+	$('.slider').slick({
+		infinite: true,
+  	appendArrows: $('.arrow'),
+  	prevArrow: '<a href="#" class="slick-arrow slick-prev"><img src="../img/Carusel/arrow left-active.png"></a>',
+  	nextArrow: '<a href="#" class="slick-arrow slick-next"><img src="../img/Carusel/arrow-right-active.png"></a>'
+	});
+	$('.slider-1').slick({
+		infinite: true,
+  	appendArrows: $('.arrow-1'),
+  	prevArrow: '<a href="#" class="slick-arrow slick-prev"><img src="../img/Carusel/arrow left-active.png"></a>',
+  	nextArrow: '<a href="#" class="slick-arrow slick-next"><img src="../img/Carusel/arrow-right-active.png"></a>'
+	});
+	$('.slider-2').slick({
+		infinite: true,
+  	appendArrows: $('.arrow-2'),
+  	prevArrow: '<a href="#" class="slick-arrow slick-prev"><img src="../img/Carusel/arrow left-mob.png"></a>',
+  	nextArrow: '<a href="#" class="slick-arrow slick-next"><img src="../img/Carusel/arrow right-mob.png"></a>'
+	});
+	$('.slider-3').slick({
+		infinite: true,
+  	appendArrows: $('.arrow-3'),
+  	prevArrow: '<a href="#" class="slick-arrow slick-prev"><img src="../img/Carusel/arrow left-mob-white.png"></a>',
+  	nextArrow: '<a href="#" class="slick-arrow slick-next"><img src="../img/Carusel/arrow right-mob-white.png"></a>'
+	});
+	$('.slider-4').slick({
+		infinite: true,
+  	appendArrows: $('.arrow-4'),
+  	prevArrow: '<a href="#" class="slick-arrow slick-prev"><img src="../img/Carusel/arrow left-mob.png"></a>',
+  	nextArrow: '<a href="#" class="slick-arrow slick-next"><img src="../img/Carusel/arrow right-mob.png"></a>'
+	});
+});
